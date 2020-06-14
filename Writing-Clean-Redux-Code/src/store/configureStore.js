b9dedbd7270e99d261215ec9ac835d2cb599ae5f
Mohib <www.muhibkhan@gmail.com>
2020-06-14 19:53:38 +0530

    redux/toolkit library to write clean code

--- Writing-Clean-Redux-Code/src/store/configureStore.js
+++ Writing-Clean-Redux-Code/src/store/configureStore.js
@@ -1,15 +1,26 @@
-import { createStore } from "redux";
-import {devToolsEnhancer} from 'redux-devtools-extension'
+import { configureStore } from "@reduxjs/toolkit";
 
 // we need not to use {} since reducer is default export
-import reducer from './bugs'
+import reducer from "./bugs";
 
-//takes reducer as function argument
-// we are just passing reducer not
-// calling it like reducer()
-//its a High order Function
+/* Using redux-toolkit library the below configureStore() can be
+mimiced and here we are importing configureStore from redux/toolkit
+and NOT importing store from redux library
+*/
+export default function () {
+  const store = configureStore({ reducer });
+  /* 
+            or
+  return configureStore({ reducer });
+   */
 
+  return store;
+}
+
+/*
 export default function configureStore(){
+  //createStore is from redux library
  const store = createStore(reducer,devToolsEnhancer({trace:true}));
  return store;
 }
+ */
